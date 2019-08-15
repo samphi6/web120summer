ec3851c76141d5fa8d0ec3466b70dad467d93e3b
samphi6 <52269653+samphi6@users.noreply.github.com>
2019-08-14 19:52:20 -0700

    Website for Lisa Marie
    navigation has changed, youtube, maps and google calendar started.

---
+++ lisa_marie/includes/footer.php
@@ -0,0 +1,44 @@
+<aside>
+        
+        <h2>UPCOMING EVENTS</h2>
+        
+        <ul>
+            <li><a href="...">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a></li>
+            <li><a href="..." >Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a></li>
+            <li><a href="..." >Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a></li>
+            <li><a href="..." >Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a></li>
+        
+        </ul>
+        
+    </aside>
+        
+    <script>  
+        
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+}
+    }
+            
+</script>
+
+</main> <!-- END OF MAIN WRAPPER -->
+
+<footer>
+        <ul>
+            <li><small><a href="#top">Back to Top</a></small></li>
+            <li><small>&copy; 2018 - <?=date('Y')?>
+            by<a href="contactme.php" target="_blank">Samuel at Coded Chrystalis</a>All Rights Reserved</small></li>
+            <li><small><a href="http://validator.w3.org/check?uri=referer" target="_blank">Validate HTML</a></small></li>
+            <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">CSS</a></li>
+        </ul>
+    
+    </footer>
+    
+    
+    
+</body>
+</html>
